360b5ba2ba4667f1c2958aff6f2f8e238f1c8a22
cathrynchang <108208717+cathrynchang@users.noreply.github.com>
2022-07-03 23:17:58 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector('.header .navbar .navlist .hamburger');
+const mobilemenu = document.querySelector('.header .navbar .navlist ul');
+const header = document.querySelector('.header.container');
+
+document.addEventListener('scroll', ()=>{
+    var scroll_position = window.scrollY;
+    if(scroll_position > 350){
+        header.style.backgroundColor = "rgb(189, 151, 110, 0.7)";
+    }else{
+        header.style.backgroundColor = "transparent";
+    }
+})
